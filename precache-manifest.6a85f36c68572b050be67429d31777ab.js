e4a5801e9026664ff207defc3f572dfc1174d3a6
Eri Shimada <shimaeri515@gmail.com>
2020-06-26 17:29:24 +0900

    Updates

---
+++ precache-manifest.6a85f36c68572b050be67429d31777ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5ab27f7309624ca71ba3dd33fa03ba0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e7dce88296c151550b60",
+    "url": "/monsters-rolodex/static/css/main.a86c051f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7dce88296c151550b60",
+    "url": "/monsters-rolodex/static/js/main.660b9f8c.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
